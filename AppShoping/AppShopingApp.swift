14da7c968e8c49644a1b633c64fa6f41ee6a2ca0
Robertto Flores <roberttozappiain@gmail.com>
2021-11-28 06:35:45 -0800

    Initial Commit

---
+++ AppShoping/AppShopingApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppShopingApp.swift
+//  AppShoping
+//
+//  Created by Robertto Flores on 28/11/21.
+//
+
+import SwiftUI
+
+@main
+struct AppShopingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
